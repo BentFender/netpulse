69de197e66247ee6f271a2b5253e48766126ba04
BentFender <dzigdzig@gmail.com>
2026-06-30 18:01:35 +0200

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -496,7 +496,7 @@ function drawLineChart(svg, series, opts) {
     const avg = seriesAverage(s.points);
     if (avg !== null) {
       const avgLabel = document.createElementNS("http://www.w3.org/2000/svg", "text");
-      avgLabel.setAttribute("x", padding.left + 6);
+      avgLabel.setAttribute("x", padding.left + 16);
       avgLabel.setAttribute("y", padding.top + 12 + avgLabelRow * 14);
       avgLabel.setAttribute("font-size", "10");
       avgLabel.setAttribute("font-weight", "600");
